b39e6f1e01a94354be5b86a528993e33b470bc55
Fucloud <270928474@qq.com>
2023-07-13 16:14:45 +0800

    修改 项目代码

--- main.py
+++ main.py
@@ -5,53 +5,54 @@
 # @Author  : raindrop
 # @Email   : 1580925557@qq.com
 # @File    : main.py
-import sys
+
 from requests import get, head
-from json import loads
+from json import loads, load
 from re import findall
-from time import time
 from os import mkdir, path
 from time import sleep, localtime, strftime, time
+import re
 import csv
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+from fmt import debug, info
 
 
 class Task(object):
 
-    def __init__(self, sec_user_id, count, tc):
+    def __init__(self, sec_user_id, download_num, thread_num, cookie):
         self.sec_user_id = sec_user_id
+        # 记录需要下载的作品个数
+        self.count = download_num
+        # 记录开启得的线程数量
+        self.tc = thread_num
+        self.cookie = cookie
+
         self.max_cursor = int(round(time() * 1000))
-        self.count = count
         self.picture = 0
         self.video = 0
         self.numb = 0
         self.nickname = "Null"
-        self.tc = tc
         self.time_start = float(round(time()))
         self.base_path = "./download"
 
     def run(self):
-        a = 1
-        while a > 0:
-            self.task()
-
-    # https://v.douyin.com/UxEKsoX/
-    def task(self):
-
         url = 'https://www.douyin.com/aweme/v1/web/aweme/post/?device_platform=webapp&aid=6383&channel=channel_pc_web&sec_user_id=' + self.sec_user_id + '&max_cursor=' + str(
             self.max_cursor) + '&locate_query=false&show_live_replay_strategy=1&count=50&publish_video_strategy_type=2&pc_client_type=1&version_code=170400&version_name=17.4.0&cookie_enabled=true&screen_width=1536&screen_height=864&browser_language=zh-CN&browser_platform=Win32&browser_name=Chrome&browser_version=108.0.5359.95&browser_online=true&engine_name=Blink&engine_version=108.0.5359.95&os_name=Windows&os_version=10&cpu_core_num=8&device_memory=8&platform=PC&downlink=10&effective_type=4g&round_trip_time=250'
-        # url = 'https://www.douyin.com/aweme/v1/web/aweme/post/?device_platform=webapp&aid=6383&channel=channel_pc_web&sec_user_id=' + self.sec_user_id + '&max_cursor=1682731628000&locate_query=false&show_live_replay_strategy=1&count=20&publish_video_strategy_type=2&pc_client_type=1&version_code=170400&version_name=17.4.0&cookie_enabled=true&screen_width=1536&screen_height=864&browser_language=zh-CN&browser_platform=Win32&browser_name=Chrome&browser_version=108.0.5359.95&browser_online=true&engine_name=Blink&engine_version=108.0.5359.95&os_name=Windows&os_version=10&cpu_core_num=8&device_memory=8&platform=PC&downlink=10&effective_type=4g&round_trip_time=250'
+
         headers = {
             'referer': 'https://www.douyin.com/user/' + self.sec_user_id,
-            'cookie': cookie(),
+            'cookie': self.cookie,
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.95 Safari/537.36'
         }
+
         resp = get(url, headers=headers)
         resp = resp.text.encode('utf-8').decode('utf-8')
         try:
+            debug(url)
             resp = loads(resp)
         except:
-            print(
+            info(
                 'cookies失效，请自行获取cookies填入脚本目录下cookie.txt中\n获取cookies方法：\n1.电脑浏览器打开抖音并登录,随便找一个人的主页打开\n2.按f12键进入开发者模式，点击网络\n3.刷新页面,网络的名称里选择第一个\n4.标头，下滑找到cookie，右键复制值')
             a = input("请输入新的cookies：")
             with open('cookie.txt', 'w+') as f:
@@ -67,24 +68,24 @@ class Task(object):
                 self.nickname = resp["aweme_list"][0]["author"]["nickname"]
                 folder_path += '/' + self.nickname
 
-                print('即将 {} 线程采集 {} 个 {} 的作品'.format(str(self.tc), str(self.count), self.nickname))
-                input('回车继续')
+                info('即将 {} 线程采集 {} 个 {} 的作品'.format(str(self.tc), str(self.count), self.nickname))
+                # input('回车继续')
                 time_start = float(round(time()))
-                print(now())
                 mkdir(folder_path + "/")
                 mkdir(folder_path + "/video/")
                 mkdir(folder_path + "/picture/")
-                print("首次创建{}缓存文件夹".format(self.nickname))
+                info("首次创建{}缓存文件夹".format(self.nickname))
             except:
-                print("{}缓存文件夹已存在".format(self.nickname))
+                info("{}缓存文件夹已存在".format(self.nickname))
+                return
 
             # 创建记录的信息
-            with open(folder_path + "/" + self.nickname + "_采集数据.csv", 'w', newline='', encoding='gbk',
+            with open(folder_path + "/" + self.nickname + "_采集数据.csv", 'w', newline='', encoding='utf-8',
                       errors='ignore') as csvfile:
                 fieldnames = ['aweme_id', '时间', 'title', '格式', '收藏', '评论', '点赞', '分享', 'share_url']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 writer.writeheader()
-        print('共{}个作品，已保存{}个，当前解析到{}'.format(str(self.count), str(self.numb), len(resp["aweme_list"])))
+        info('共{}个作品，已保存{}个，当前解析到{}'.format(str(self.count), str(self.numb), len(resp["aweme_list"])))
 
         # 根据需求截取信息
         if self.count == '∞':
@@ -93,25 +94,24 @@ class Task(object):
             aweme_list = resp["aweme_list"][:(int(self.count) - int(self.numb))]
         else:
             aweme_list = resp["aweme_list"]
+
         pool = ThreadPoolExecutor(self.tc)
+
         for aweme in aweme_list:
             pool.submit(self.download, folder_path, aweme)
             self.numb = self.numb + 1
         pool.shutdown()
+
         if str(self.numb) == str(self.count):
-            print("已采集指定数目作品,共{}个作品,{}个视频，{}个图片，请在脚本目录下查看".format(self.numb, self.video, self.picture))
+            info("已采集指定数目作品,共{}个作品,{}个视频，{}个图片，请在脚本目录下查看".format(self.numb, self.video, self.picture))
             self.time_cha()
-            input('回车退出')
-            exit()
         if resp["has_more"] == 0:
-            print("数据采集结束,共{}个作品,{}个视频，{}个图片，请在脚本目录下查看".format(self.numb, self.video, self.picture))
+            info("数据采集结束,共{}个作品,{}个视频，{}个图片，请在脚本目录下查看".format(self.numb, self.video, self.picture))
             self.time_cha()
-            input('回车退出')
-            exit()
         self.max_cursor = resp["max_cursor"]
 
     def time_cha(self):
-        print('运行结束')
+        info('运行结束')
         time_end = float(round(time()))
         time_diff = int(time_end - self.time_start)
         if time_diff >= 3600:
@@ -126,14 +126,11 @@ class Task(object):
             mm = 0
         if time_diff > 0:
             ss = time_diff
-        print(now())
-        print('本次执行共耗时{}时{}分{}秒'.format(str(hh), str(mm), str(ss)))
+        info('本次执行共耗时{}时{}分{}秒'.format(str(hh), str(mm), str(ss)))
 
     def download(self, folder_path: str, aweme):
         try:
-            print(aweme['desc'])
             desc = aweme["statistics"]
-            print("[debug] desc: ", desc)
             desc['收藏'] = desc.pop('collect_count')
             desc['评论'] = desc.pop('comment_count')
             desc['点赞'] = desc.pop('digg_count')
@@ -142,7 +139,7 @@ class Task(object):
             desc['share_url'] = aweme['share_info']['share_url']
         except Exception as e:
             print("[debug] Exception: ", repr(e))
-        if aweme['images'] == None:
+        if aweme['images'] is None:
             desc['格式'] = "video"
         else:
             desc['格式'] = "picture"
@@ -154,22 +151,24 @@ class Task(object):
         # 转换成新的时间格式
         desc['时间'] = strftime("%Y-%m-%d %H:%M:%S", time_2)
         desc['title'] = aweme['desc']
-        with open(folder_path + "/" + self.nickname + "_采集数据.csv", 'a', newline='', encoding='gbk',
+        with open(folder_path + "/" + self.nickname + "_采集数据.csv", 'a', newline='', encoding='utf-8',
                   errors='ignore') as csvfile:
             fieldnames = ['aweme_id', '时间', 'title', '格式', '收藏', '评论', '点赞', '分享', 'share_url']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow(desc)
-        print(str(desc))
+
         # 视频
-        print('-------------------------------------')
-        if aweme['images'] == None:
+        if aweme['images'] is None:
             url = aweme["video"]["play_addr"]["url_list"][0]
-            print(url)
-            video = get(url)
-            # 添加基础路径
-            with open(folder_path + '/' + aweme["author"]["nickname"] + "/video/" + aweme["aweme_id"] + '.mp4', 'wb') as f:
-                f.write(video.content)
-            self.video += 1
+            try:
+                video = get(url)
+                # 添加基础路径
+                download_path = folder_path + "/video/"
+                with open(download_path + aweme["aweme_id"] + '.mp4', 'wb') as f:
+                    f.write(video.content)
+                self.video += 1
+            except Exception as e:
+                debug(repr(e))
         else:
             url_list = aweme["images"]
             s = 0
@@ -192,50 +191,47 @@ def now():
     return nows
 
 
+# 读取url列表
+def read_url_list(file_path: str) -> list[str]:
+    url_list = []
+
+    # 打开存储URL地址的文件 并读取
+    with open(file_path) as f:
+        read_data = f.readlines(-1)
+        for url in read_data:
+            if re.match("https://www.douyin.com/user/*", url):
+                url_list.append(url.strip())
+
+        f.close()
+
+    return url_list
+
+
+def read_config_info(file_path: str) -> dict:
+    with open(file_path) as f:
+        config_info = load(f)
+        f.close()
+        return config_info
+
+
 def main():
-    print(now())
-    print("github开源地址:https://github.com/raindrop-hb/douyin_spider\n使用请保留版权\n欢迎使用raindrop抖音爬虫_解析工具")
-    ex = 1
-    try:
-        aa = get("", timeout=4)
-        aa = aa.json()
-        if len(aa['url']) > 0:
-            for i in aa['url']:
-                get(i)
-        if aa["version"] > 1.03:
-            print("检测到更新:\n更新版本号:{}\n更新内容:{}".format(aa["version"], aa["content"]))
-            cc = get(aa["download"])
-            with open('抖音爬虫' + str(aa["version"]) + '.exe', 'wb') as f:
-                f.write(cc.content)
-            if int(aa["force"]) == 1:
-                print('当前版本为强制更新，已下载到工具运行目录下')
-                ex = 0
-            else:
-                print('已下载到工具运行目录下')
-        else:
-            print('当前为新版')
-    except:
-        print('连接更新服务器失败')
-    if ex:
-        a = input('输入主页链接：')
-        b = input('请输入要采集的作品数,为1即解析最近更新的,其他数即从现在往上爬取,直接回车即爬取全部作品\n请输入:')
-        while True:
-            d = input('输入线程数\n回车默认4:')
-            if not d.isdigit():
-                d = 4
-                break
-            elif int(d) >= 1:
-                break
-        if not path.exists("cookie.txt"):
-            with open('cookie.txt', 'w+') as f:
-                # 这里填cookie
-                c = ''
-                f.write(c)
-            print("使用默认cookie，若后续程序无法执行，请自行抓取cookie")
-            sleep(3)
-        if b == '':
-            b = '∞'
-        a = 'https' + findall('https(.*)', a)[0]
+    debug(now())
+    info("github开源地址:https://github.com/raindrop-hb/douyin_spider\n使用请保留版权\n欢迎使用raindrop抖音爬虫_解析工具")
+
+    # 读取配置信息
+    config_info = read_config_info("./config.json")
+
+    # 读取URL地址
+    user_urls = read_url_list(config_info["urls_path"])
+    if len(user_urls) <= 0:
+        info("无链接")
+
+    thread_num = config_info["thread_num"]
+    download_num = config_info["download_num"]
+    cookie = read_cookie(config_info["cookie_path"])
+
+    for url in user_urls:
+        a = 'https' + findall('https(.*)', url)[0]
         # a = head(a)
         # headers={
         #     "cookie":cookie()
@@ -244,16 +240,12 @@ def main():
         # a = head(a,headers=headers).headers['Location']
         a = a.replace('https://www.douyin.com/user/', '').replace('?previous_page=web_code_link', '').replace(
             '?previous_page=app_code_link', '')
-        c = Task(a, b, int(d))
-        c.run()
-
-    else:
-        input('回车退出')
-        exit(0)
+        task = Task(a, download_num, thread_num, cookie)
+        task.run()
 
 
-def cookie():
-    with open('cookie.txt', 'r') as f:
+def read_cookie(cookie_path: str):
+    with open(cookie_path, 'r') as f:
         c = f.read()
     return str(c)
 
